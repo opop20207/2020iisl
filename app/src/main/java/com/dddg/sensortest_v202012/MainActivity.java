e912f4783e30f2440e48fa6c08d3d87498011511
opop20207 <opop202077@gmail.com>
2020-12-21 21:41:48 +0900

    init

---
+++ app/src/main/java/com/dddg/sensortest_v202012/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dddg.sensortest_v202012;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
