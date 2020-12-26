3cd72b44000a25e4d0039de4378cd49bf73413e9
eonju <deer8203@naver.com>
2020-12-26 16:20:59 +0900

    12/26 updated MainActivity.java

--- app/src/main/java/com/dddg/sensortest_v202012/MainActivity.java
+++ app/src/main/java/com/dddg/sensortest_v202012/MainActivity.java
@@ -13,11 +13,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
+/**
+ * Comments 2020-12-26
+ * Pass three values from MainActivity.java to WriteActivity.class
+ *  1. fileName - name of file (파일명)
+ *  2. timeInterval - interval between two rows of record (파일 출력 간격)
+ *  3. s - delay of sensor (센서 딜레이)
+ */
 public class MainActivity extends AppCompatActivity {
+
+    // Widgets
     Button btnStart;
     RadioGroup rg;
     EditText etxtFileName, etxtTimeInterval;
+
+    // permissions
+    boolean fileRead=false, fileWrite=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,31 +42,42 @@ public class MainActivity extends AppCompatActivity {
         etxtFileName = (EditText) findViewById(R.id.etxtFileName);
         etxtTimeInterval = (EditText) findViewById(R.id.etxtTimeInterval);
 
+        // check permissions
         checkFilePermission();
+
+        // if the user allow permissions, move to writeActivity
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), WriteActivity.class);
-                intent.putExtra("fileName", etxtFileName.getText().toString());
-                intent.putExtra("timeInterval", Integer.parseInt(etxtTimeInterval.getText().toString()));
-                int s=0;
-                if(rg.getCheckedRadioButtonId() == R.id.rbtn1){
-                    s = SensorManager.SENSOR_DELAY_FASTEST;
-                }else if(rg.getCheckedRadioButtonId() == R.id.rbtn2){
-                    s = SensorManager.SENSOR_DELAY_GAME;
-                }else if(rg.getCheckedRadioButtonId() == R.id.rbtn3){
-                    s = SensorManager.SENSOR_DELAY_UI;
-                }else{
-                    s = SensorManager.SENSOR_DELAY_NORMAL;
+                if(fileRead&&fileWrite){
+                    goToWriteActivity();
                 }
-                intent.putExtra("sensorSpeed", s);
-                startActivity(intent);
+                else{
+                    checkFilePermission();
+                }
             }
         });
     }
 
+    public void goToWriteActivity(){
+        Intent intent = new Intent(getApplicationContext(), WriteActivity.class);
+        intent.putExtra("fileName", etxtFileName.getText().toString());
+        intent.putExtra("timeInterval", Integer.parseInt(etxtTimeInterval.getText().toString()));
+        int s=0;
+        if(rg.getCheckedRadioButtonId() == R.id.rbtn1){
+            s = SensorManager.SENSOR_DELAY_FASTEST;
+        }else if(rg.getCheckedRadioButtonId() == R.id.rbtn2){
+            s = SensorManager.SENSOR_DELAY_GAME;
+        }else if(rg.getCheckedRadioButtonId() == R.id.rbtn3){
+            s = SensorManager.SENSOR_DELAY_UI;
+        }else{
+            s = SensorManager.SENSOR_DELAY_NORMAL;
+        }
+        intent.putExtra("sensorSpeed", s);
+        startActivity(intent);
+    }
+
     public void checkFilePermission(){
-        boolean fileRead=false, fileWrite=false;
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
             fileRead=true;
         }
